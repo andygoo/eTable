a65ee966bc7d04f96f7fedd11e1e49a7d238a0ae
Gina <sealdead@gmail.com>
2012-10-10 22:57:35 +0800

    add delete col method

--- src/jquery.eTable.js
+++ src/jquery.eTable.js
@@ -67,33 +67,33 @@
 		 *
 		 * @param rowIndex
 		 *            {Number}
-		 * @param tds
+		 * @param cells
 		 *            {Array}
 		 */
-		this.__insertRow = function(rowIndex, tds) {
-			if (!$.isArray(tds) || tds.length === 0) {//参数tds为长度不为0的数组
+		this.__insertRow = function(rowIndex, cells) {
+			if (!$.isArray(cells) || cells.length === 0) {// 参数tds为长度不为0的数组
 				console.error("table cell data type should be an array and length of not less then 0!");
 				return false;
 			}
-			if (rowIndex < -1) {//插入行位置不能小于-1
+			if (rowIndex < -1) {// 插入行位置不能小于-1
 				console.error("The rowIndex (" + rowIndex + ") can not be less than -1 !");
 				return false;
 			}
 
 			var rows = this.getRow();
 
-			if (rowIndex > rows) {//插入行位置不能大于当前总行数
+			if (rowIndex > rows) {// 插入行位置不能大于当前总行数
 				console.error("The " + ordinal(rowIndex + 1) + " row can not be inserted, because the table is only " + rows + " rows");
 				return false;
 			}
 			console.info('rows: ' + this.getRow() + ' rowIndex: ' + rowIndex);
 			var tr = this.table[0].insertRow(rowIndex);
 
-			$.each(tds, function(idx, td) {
-				if ( td instanceof jQuery) {
-					$(tr.insertCell(idx)).append(td);
+			$.each(cells, function(idx, cell) {
+				if ( cell instanceof jQuery) {
+					$(tr.insertCell(idx)).append(cell);
 				} else {
-					tr.insertCell(idx).innerHTML = td;
+					tr.insertCell(idx).innerHTML = cell;
 				}
 			});
 		};
@@ -102,96 +102,120 @@
 		 *
 		 * @param colIndex
 		 *            {Number}
-		 * @param tds
+		 * @param cells
 		 *            {Array}
 		 */
-		this.__insertCol = function(colIndex, tds) {
-			if (!$.isArray(tds) || tds.length === 0) {//参数tds为长度不为0的数组
+		this.__insertCol = function(colIndex, cells) {
+			if (!$.isArray(cells) || cells.length === 0) {// 参数tds为长度不为0的数组
 				console.error("table cell data type should be an array and length of not less then 0!");
 				return false;
 			}
-			if (colIndex < -1) {//插入列位置不能小于-1
+			if (colIndex < -1) {// 插入列位置不能小于-1
 				console.error("The colIndex (" + colIndex + ") can not be less than -1 !");
 				return false;
 			}
 
 			var self = this, rows = this.table[0].rows;
 
-			$.each(tds, function(idx, td) {
+			$.each(cells, function(idx, cell) {
 				var row = rows[idx];
 				if (!row) {
 					row = $("<tr>")[0];
 					self.table.append(row);
 				}
 
-				var cells = row.cells.length;
+				var iCell = row.cells.length;
 
-				if (colIndex > cells) {//插入单元格位置不能大于当前行所包含的单元格
-					console.warn("the " + ordinal(colIndex + 1) + " cell can not be append, because the " + ordinal(idx + 1) + " row only " + cells + " cells");
+				if (colIndex > iCell) {// 插入单元格位置不能大于当前行所包含的单元格数量
+					console.warn("the " + ordinal(colIndex + 1) + " cell can not be append, because the " + ordinal(idx + 1) + " row only " + iCell + " cells");
 				} else {
-
-					if ( td instanceof jQuery) {
-						$(row.insertCell(colIndex)).append(td);
+					if ( cell instanceof jQuery) {
+						$(row.insertCell(colIndex)).append(cell);
 					} else {
-						row.insertCell(colIndex).innerHTML = td;
+						row.insertCell(colIndex).innerHTML = cell;
 					}
 				}
 			});
 		};
 		/**
 		 * Pirvate Method Delete Row
+		 * <ul>
+		 * <li>rowIndex = 0 : first row</li>
+		 * <li>rowIndex = -1 : last row</li>
+		 * </ul>
 		 *
 		 * @param rowIndex
 		 *            {Number}
 		 *
 		 */
 		this.__deleteRow = function(rowIndex) {
-			if (rowIndex === -1 || this.getRow() <= rowIndex) {
-				alert("Delete rows does not exist");
+			if (this.getRow() === 0) {
+				console.warn("Empty table !");
+				return;
+			}
+			if (rowIndex > this.getRow() - 1) {
+				console.warn("the " + ordinal(rowIndex + 1) + " rows does not exist ");
 				return;
 			}
 			this.table[0].deleteRow(rowIndex);
 			console.info('rows: ' + this.getRow() + ' rowIndex: ' + rowIndex);
 		};
 
+		this.__deleteCol = function(colIndex) {
+			if (this.getRow() === 0) {
+				console.warn("Empty table !");
+				return;
+			}
+			var rows = this.table[0].rows;
+			for (var r = rows.length - 1; r > -1; r--) {
+				var iCell = rows[r].cells.length;
+				if (colIndex > iCell - 1) {
+					console.error("the " + ordinal(colIndex + 1) + " cols does not exist");
+					break;
+				}
+				rows[r].deleteCell(colIndex);
+				if (iCell === 1) {
+					this.__deleteRow(r);
+				}
+			}
+		}
 		/**
 		 * Append a new row
 		 *
-		 * @param tds
+		 * @param cells
 		 *            {Array}
 		 *
 		 */
-		this.appendRow = function(tds) {
-			//this.__insertRow(this.getRow(), tds);
-			this.__insertRow(-1, tds);
+		this.appendRow = function(cells) {
+			// this.__insertRow(this.getRow(), cells);
+			this.__insertRow(-1, cells);
 		};
 
 		/**
 		 * Insert into the 1st row
 		 *
-		 * @param tds
+		 * @param cells
 		 *            {Array}
 		 */
-		this.insertToFirstRow = function(tds) {
-			this.__insertRow(0, tds);
+		this.insertToFirstRow = function(cells) {
+			this.__insertRow(0, cells);
 		};
+
 		/**
 		 * Insert into the last row
-		 *
-		 * @param tds
-		 *            {Array}
 		 */
 		this.insertToLastRow = this.appendRow;
+
 		/**
 		 * Inserted before the specified row
 		 *
 		 * @param rowIndex
 		 *            {Number}
-		 * @param tds
+		 * @param cells
 		 *            {Array}
 		 */
-		this.insertBeforeRow = function(rowIndex, tds) {
-			this.__insertRow(rowIndex - 1, tds);
+		this.insertBeforeRow = function(rowIndex, cells) {
+			this.__insertRow(rowIndex - 1, cells);
 		};
 
 		/**
@@ -199,11 +223,11 @@
 		 *
 		 * @param rowIndex
 		 *            {Number}
-		 * @param tds
+		 * @param cells
 		 *            {Array}
 		 */
-		this.insertAfterRow = function(rowIndex, tds) {
-			this.__insertRow(rowIndex, tds);
+		this.insertAfterRow = function(rowIndex, cells) {
+			this.__insertRow(rowIndex, cells);
 		};
 
 		/**
@@ -217,7 +241,7 @@
 		 * delete the last row
 		 */
 		this.deleteLastRow = function() {
-			this.__deleteRow(this.getRow() - 1);
+			this.__deleteRow(-1);
 		};
 
 		/**
@@ -237,25 +261,38 @@
 		 *            {Array}
 		 */
 		this.deleteRows = function(rowIndexs) {
-			var row = rowIndexs.length;
-			for (var r = row - 1; r > -1; r--) {
+			for (var r = rowIndexs.length - 1; r > -1; r--) {
 				this.__deleteRow(rowIndexs[r]);
 			}
 		};
 
-		this.appendCol = function(tds) {
-			this.__insertCol(-1, tds);
+		this.appendCol = function(cells) {
+			this.__insertCol(-1, cells);
 		};
-		this.insertToFirstCol = function(tds) {
-			this.__insertCol(0, tds);
+		this.insertToFirstCol = function(cells) {
+			this.__insertCol(0, cells);
 		};
 		this.insertToLastCol = this.appendCol;
 
-		this.insertBeforeCol = function(colIndex, tds) {
-			this.__insertCol(colIndex - 1, tds);
-		}
-		this.insertAfterCol = function(colIndex, tds) {
-			this.__insertCol(colIndex, tds);
+		this.insertBeforeCol = function(colIndex, cells) {
+			this.__insertCol(colIndex, cells);
+		};
+		this.insertAfterCol = function(colIndex, cells) {
+			this.__insertCol(colIndex, cells);
+		};
+		this.deleteFirstCol = function() {
+			this.__deleteCol(0);
+		};
+		this.deleteLastCol = function() {
+			this.__deleteCol(-1);
+		};
+		this.deleteCol = function(colIndex) {
+			this.__deleteCol(colIndex);
+		};
+		this.deleteCols = function(colIndexs) {
+			for (var ci = colIndexs.length - 1; ci > -1; ci--) {
+				this.__deleteCol(colIndexs[ci]);
+			}
 		}
 	};
 
